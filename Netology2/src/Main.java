f84e28412f3edd9d2e25a5d796879680aa0b7847
IOCAcorporation <hri.st@yandex.com>
2025-08-01 14:57:21 +0300

    Add files via upload

---
+++ Netology2/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int Ticket =13000;
+        int Bonus = Ticket / 20;
+        int Answer = Bonus;
+        System.out.println("За билет начисляется -");
+        System.out.println((Answer + " Миль"));
+
+
+
+    }
+}
